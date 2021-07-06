aa45813486fe183508428b5a680cbf7dcabb897f
Sheng Li <sheng.li@plus.ai>
2021-07-06 13:32:45 -0700

    udpate

--- data_utils/process_highd/process_highd.py
+++ data_utils/process_highd/process_highd.py
@@ -20,7 +20,7 @@ from args import args
 RAW_DATASET_PATH = '../../data/raw_data/highd/data/'
 PROCESSED_DATASET_PATH = '../../data/processed_data/highd/{}_uuid_to_track.pickle'
 ANIMATION_PATH = '../../data/processed_data/highd/{}_{}_fps={}_animation.mp4'
-TORCH_TENSOR_PATH = '../../data/processed_data/highd/input={}_pred={}_stride={}/{}_{}.pt'
+TORCH_TENSOR_DIR = '../../data/processed_data/highd/input={}_pred={}_stride={}'
 
 def create_args():
     parser = argparse.ArgumentParser()
@@ -228,14 +228,16 @@ def generate_training_data_segments(dataset_list=None):
         input_edge_types_segments = torch.tensor(input_edge_types_segments).float()
         pred_seq_seqments = torch.tensor(pred_seq_seqments).float()
 
-        torch.save(input_seq_segments, TORCH_TENSOR_PATH.format(
-            args.input_seconds, args.pred_seconds, args.forward_shift_seconds, data_str, 'input_seq'))
-        torch.save(input_masks_segments, TORCH_TENSOR_PATH.format(
-            args.input_seconds, args.pred_seconds, args.forward_shift_seconds, data_str, 'input_masks'))
-        torch.save(input_edge_types_segments, TORCH_TENSOR_PATH.format(
-            args.input_seconds, args.pred_seconds, args.forward_shift_seconds, data_str, 'input_edge_types'))
-        torch.save(pred_seq_seqments, TORCH_TENSOR_PATH.format(
-            args.input_seconds, args.pred_seconds, args.forward_shift_seconds, data_str, 'pred_seq'))
+        torch_tensor_dir = TORCH_TENSOR_DIR.format(
+            args.input_seconds, args.pred_seconds, args.forward_shift_seconds)
+        if not os.path.exists(torch_tensor_dir):
+            os.makedirs(torch_tensor_dir)
+
+        torch_tensor_path = torch_tensor_dir + '/{}_{}.pt'
+        torch.save(input_seq_segments, torch_tensor_path.format(data_str, 'input_seq'))
+        torch.save(input_masks_segments, torch_tensor_path.format(data_str, 'input_masks'))
+        torch.save(input_edge_types_segments, torch_tensor_path.format(data_str, 'input_edge_types'))
+        torch.save(pred_seq_seqments, torch_tensor_path.format(data_str, 'pred_seq'))
 
 if __name__ == '__main__':
     args = create_args()
